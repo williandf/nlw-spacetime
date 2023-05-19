b29a63883cf69f2c64bbb6569c9f4dc70cc3e015
Willian Del Freu <williankeyex@gmail.com>
2023-05-19 00:21:20 -0300

    Aula 4 Finalizada

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 import { authRoutes } from './routes/auth'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads/'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // todas as URLs de front-end poderão acessar nosso backend
@@ -16,6 +26,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
